52bf915618dfe51b366c30ab763331c79cf4bae4
1prabhash1 <98809684+1prabhash1@users.noreply.github.com>
2026-04-01 16:22:06 +0530

    Update non-auth-user.py

--- bot/plugins/non-auth-user.py
+++ bot/plugins/non-auth-user.py
@@ -1,6 +1,6 @@
 import logging
 
-from pyrogram import filters as Filters
+from pyrogram import filters
 from pyrogram.types import Message
 
 from ..utubebot import UtubeBot
@@ -11,10 +11,10 @@ log = logging.getLogger(__name__)
 
 
 @UtubeBot.on_message(
-    Filters.private & Filters.incoming & ~Filters.user(Config.AUTH_USERS)
+    filters.private & filters.incoming & ~filters.user(Config.AUTH_USERS)
 )
 async def _non_auth_usr_msg(c: UtubeBot, m: Message):
-    await m.delete(True)
+    await m.delete()
     log.info(
-        f"{Config.AUTH_USERS} Unauthorised user {m.chat} contacted. Message {m} deleted!!"
+        f"{Config.AUTH_USERS} Unauthorised user {m.chat.id} contacted. Message deleted."
     )
