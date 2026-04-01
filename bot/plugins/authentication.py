21d5c613fccfb53b6af76dbbac7f113e5c120ed2
1prabhash1 <98809684+1prabhash1@users.noreply.github.com>
2026-04-01 16:43:04 +0530

    Update authentication.py

--- bot/plugins/authentication.py
+++ bot/plugins/authentication.py
@@ -1,6 +1,6 @@
 import logging
 
-from pyrogram import filters as Filters
+from pyrogram import filters
 from pyrogram.types import Message
 
 from ..youtube import GoogleAuth
@@ -13,14 +13,15 @@ log = logging.getLogger(__name__)
 
 
 @UtubeBot.on_message(
-    Filters.private
-    & Filters.incoming
-    & Filters.command("authorise")
-    & Filters.user(Config.AUTH_USERS)
+    filters.private
+    & filters.incoming
+    & filters.command("authorise")
+    & filters.user(Config.AUTH_USERS)
 )
 async def _auth(c: UtubeBot, m: Message) -> None:
-    if len(m.command) == 1:
-        await m.reply_text(tr.NO_AUTH_CODE_MSG, True)
+
+    if len(m.command) < 2:
+        await m.reply_text(tr.NO_AUTH_CODE_MSG)
         return
 
     code = m.command[1]
@@ -29,37 +30,38 @@ async def _auth(c: UtubeBot, m: Message) -> None:
         auth = GoogleAuth(Config.CLIENT_ID, Config.CLIENT_SECRET)
 
         auth.Auth(code)
-
         auth.SaveCredentialsFile(Config.CRED_FILE)
 
-        msg = await m.reply_text(tr.AUTH_SUCCESS_MSG, True)
+        msg = await m.reply_text(tr.AUTH_SUCCESS_MSG)
 
         with open(Config.CRED_FILE, "r") as f:
             cred_data = f.read()
 
-        log.debug(f"Authentication success, auth data saved to {Config.CRED_FILE}")
+        log.debug(f"Authentication success, saved to {Config.CRED_FILE}")
 
         msg2 = await msg.reply_text(cred_data, parse_mode=None)
+
         await msg2.reply_text(
-            "This is your authorisation data! Save this for later use. Reply /save_auth_data to the authorisation "
-            "data to re authorise later. (helpful if you use Heroku)",
-            True,
+            "This is your authorization data! Save this safely.\n"
+            "Reply /save_auth_data to this message to restore later."
         )
 
     except Exception as e:
         log.error(e, exc_info=True)
-        await m.reply_text(tr.AUTH_FAILED_MSG.format(e), True)
+        await m.reply_text(tr.AUTH_FAILED_MSG.format(e))
 
 
 @UtubeBot.on_message(
-    Filters.private
-    & Filters.incoming
-    & Filters.command("save_auth_data")
-    & Filters.reply
-    & Filters.user(Config.AUTH_USERS)
+    filters.private
+    & filters.incoming
+    & filters.command("save_auth_data")
+    & filters.reply
+    & filters.user(Config.AUTH_USERS)
 )
 async def _save_auth_data(c: UtubeBot, m: Message) -> None:
+
     auth_data = m.reply_to_message.text
+
     try:
         with open(Config.CRED_FILE, "w") as f:
             f.write(auth_data)
@@ -68,8 +70,10 @@ async def _save_auth_data(c: UtubeBot, m: Message) -> None:
         auth.LoadCredentialsFile(Config.CRED_FILE)
         auth.authorize()
 
-        await m.reply_text(tr.AUTH_DATA_SAVE_SUCCESS, True)
-        log.debug(f"Authentication success, auth data saved to {Config.CRED_FILE}")
+        await m.reply_text(tr.AUTH_DATA_SAVE_SUCCESS)
+
+        log.debug(f"Auth restored from {Config.CRED_FILE}")
+
     except Exception as e:
         log.error(e, exc_info=True)
-        await m.reply_text(tr.AUTH_FAILED_MSG.format(e), True)
+        await m.reply_text(tr.AUTH_FAILED_MSG.format(e))
